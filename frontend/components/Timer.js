21b4ec8b3f5b67968b6aa23d6e8e6aa3c78570d3
apmfree78 <amit@profitswami.com>
2022-05-27 17:31:06 -0400

    added hour display to timer

--- frontend/components/Timer.js
+++ frontend/components/Timer.js
@@ -3,11 +3,19 @@ import { Typography } from '@mui/material';
 import React from 'react';
 
 // take seconds and returns mm:ss format as string
-const displayTime = (seconds) =>
-  `${Math.floor(seconds / 60)
+const displayTime = (time) => {
+  const hours = Math.floor(time / 3600); // whole number of hours
+  const rawMinutes = (time % 3600) / 60; // minutes as floating point number
+  const minutes = Math.floor(rawMinutes); // whole number of minutes
+  const seconds = Math.round((rawMinutes - minutes) * 60); // seconds
+  if (hours > 0)
+    return `${hours.toString()}:${minutes.toString().padStart(2, '0')}:${seconds
+      .toString()
+      .padStart(2, '0')}`;
+  return `${minutes.toString().padStart(2, '0')}:${seconds
     .toString()
-    .padStart(2, '0')}:${(seconds % 60).toString().padStart(2, '0')}`;
-
+    .padStart(2, '0')}`;
+};
 // show current time on timer and mode
 // note current time is passed down as children from ReminderCard.js
 const Timer = ({ children }) => (
