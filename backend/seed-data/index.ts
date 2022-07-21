460cd6f181935a3414b5841215622fb7e2a7818e
apmfree78 <amit@profitswami.com>
2022-07-20 21:41:34 -0400

    deploy 5

--- backend/seed-data/index.ts
+++ backend/seed-data/index.ts
@@ -1,5 +1,6 @@
 import { products } from './data';
 
+// add seed data
 export async function insertSeedData(ks: any) {
   // Keystone API changed, so we need to check for both versions to get keystone
   const keystone = ks.keystone || ks;
@@ -10,12 +11,12 @@ export async function insertSeedData(ks: any) {
   for (const product of products) {
     console.log(`  🛍️ Adding Product: ${product.name}`);
     const { _id } = await mongoose
-    .model('ProductImage')
-    .create({ image: product.photo, altText: product.description });
+      .model('ProductImage')
+      .create({ image: product.photo, altText: product.description });
     product.photo = _id;
     await mongoose.model('Product').create(product);
   }
   console.log(`✅ Seed Data Inserted: ${products.length} Products`);
-  console.log(`👋 Please start the process with \`yarn dev\` or \`npm run dev\``);
+  console.log('👋 Please start the process with `yarn dev` or `npm run dev`');
   process.exit();
 }
